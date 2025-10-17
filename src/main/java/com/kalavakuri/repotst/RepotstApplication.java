5247096e00ce3b00baefd6893f67179e18de7dfa
rkalavak <kalavakurirenu@gmail.com>
2025-10-17 18:57:57 +0530

    Initial commit

---
+++ src/main/java/com/kalavakuri/repotst/RepotstApplication.java
@@ -0,0 +1,13 @@
+package com.kalavakuri.repotst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepotstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepotstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalavakuri/repotst/RepotstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalavakuri.repotst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepotstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
